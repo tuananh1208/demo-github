f427e63bd3a66321089cc6ceb0eb3ac596b479db
tuananh1208 <trantuananh120803@gmail.com>
2022-09-18 09:13:08 +0700

    Add introduction

--- demo_file.cpp
+++ demo_file.cpp
@@ -6,13 +6,8 @@ using ll = long long;
 
 
 int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     
 
     return 0;
